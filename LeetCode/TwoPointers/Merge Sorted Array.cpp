748767a411b7c0375423fa8afa6b723deafa9512
Kushal S <kushal17062004@gmail.com>
2026-03-21 23:16:18 +0530

    Merge Array Problem

---
+++ LeetCode/TwoPointers/Merge Sorted Array.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        int size=m+n-1;
+        int idx1=m-1,idx2=n-1;
+        while(idx1>=0 && idx2>=0){
+            if(nums1[idx1]<=nums2[idx2]){
+                nums1[size--]=nums2[idx2--];
+            }else{
+                nums1[size--]=nums1[idx1--];
+            }
+        }
+        while(idx2>=0){
+            nums1[size--]=nums2[idx2--];
+        }
+    }
+};
